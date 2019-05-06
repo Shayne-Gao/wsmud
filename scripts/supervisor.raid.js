9c5330caa8530493793638f9eed569eaf83559d3
ShayneGao <gwlj@outlook.com>
2019-05-06 15:04:47 +0800

    readme

--- scripts/supervisor.raid.js
+++ scripts/supervisor.raid.js
@@ -1,10 +1,10 @@
 //状态监控发微信
 ($learnPotMin) = 10000
-($successBuff) = force
+($successBuff) = success
 ($lastState)=(:state)
 
 
-($hasSuccessBefore) = (:status (successBuff))
+($hasSuccessBefore) = false
  @toolbar score
 @js ($menpai)=$("[data-prop='family']").text()
  @js ($pot)=$("[data-prop='pot']").text()
@@ -23,15 +23,15 @@
     [if] (:status (successBuff)) == true
         [if] (hasSuccessBefore) = false
         //新获得鼓舞
+            @print 新获得鼓舞
             ($hasSuccessBefore) = true
              @toolbar score
-             @dialog
              @js ($pot)=$("[data-prop='pot']").text()
             [if] (pot) > (learnPotMin)
                 //潜能足够 去学习
                 ($logStr) =(logStr)\n\r(:hour):(:minute):(:second) 获得鼓舞,潜能足够,去学习
                 @print 获得鼓舞!去学习
-                //@call 4学习
+                @call 4学习
             [else]
                 ($logStr) =(logStr)\n\r(:hour):(:minute):(:second) 获得鼓舞,潜能不足,去打坐
                 @print 获得鼓舞!潜能不够,去打坐
@@ -49,11 +49,24 @@
         $wait 30000
         [if] (:state)== 发呆
             [if] (:combating) == false
-                @当前正在发呆
-                ($logStr) =(logStr)\n\r(:hour):(:minute):(:second) 检测到发呆,去挖矿
+                @print 当前正在发呆
+                ($logStr) =(logStr)\n\r(:hour):(:minute):(:second) 检测到在(:room)发呆,去挖矿
+                   [if] (:status (successBuff)) == true
+                        @toolbar score
+                        @js ($pot)=$("[data-prop='pot']").text()
+                        [if] (pot) > (learnPotMin)
+                            //潜能足够 去学习
+                            ($logStr) =(logStr)\n\r(:hour):(:minute):(:second) 发呆时候有鼓舞,潜能足够,去学习
+                            @print 获得鼓舞!去学习
+                            @call 4学习
+                        [else]
+                            ($logStr) =(logStr)\n\r(:hour):(:minute):(:second) 发呆时候有鼓舞,潜能不足,去打坐
+                            @print 获得鼓舞!潜能不够,去打坐
+                            $to 帮会-练功房
+                            dazuo
                 $zdwk
             [else]
-                @战斗中..
+                @print 战斗中..
                 ($logStr) =(logStr)\n\r(:hour):(:minute):(:second) 检测到战斗中
 
     ($nowState) = (:state)
@@ -75,6 +88,9 @@
         ($alive) = 凉了
     @print <hig>(menpai)当前<(:state)>at<(:room)>,潜能(pot),(hasSuccess),气血(:hp)/(:maxHp),(alive)</hig>
 
+    //push post
+    @js $.post("http://119.29.138.20/api/wsmud_recordd", {role_id:"(:id)",role_name: "unkonw", role_menpai: "(menpai)" ,state:"(:state)",last_state:"(lastState)",hp:"(:hp)",maxhp:"(:maxHp)",mp:"(:mp)",maxMp:"(:maxMp)",has_success_buff:"(:status (successBuff))",pot:"(pot)",isalive:"(:living)",iscombating:"(:combating)"})
+
     @print (logStr)
     //处理通知
     [if] (logStr) != 日志
@@ -86,3 +102,4 @@
         @print (title)
         @print 准备发送wechat提醒
         @js $.post("https://sc.ftqq.com/SCU15524T3b74cdf9889582dd05f0f6311c30daa95a02d41d5e091.send", { text: "(title)", desp: "(body)" })
+
